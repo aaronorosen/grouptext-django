b70f0b9e70d1bff2c3a9193239dcd3cc6efc9d05
Aaron Rosen <aaronorosen@gmail.com>
2020-10-15 10:48:30 -0700

    move these to env

--- codes/web/settings.py
+++ codes/web/settings.py
@@ -53,9 +53,9 @@ REST_FRAMEWORK = {
 
 
 TWILIO = {
-    'TWILIO_ACCOUNT_SID': '',
-    'TWILIO_AUTH_TOKEN': '',
-    'TWILIO_NUMBER': '',
+    'TWILIO_ACCOUNT_SID': os.environ.get('TWILIO_ACCOUNT_SID'),
+    'TWILIO_AUTH_TOKEN': os.environ.get('TWILIO_AUTH_TOKEN'),
+    'TWILIO_NUMBER': os.environ.get('TWILIO_NUMBER'),
 }
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
